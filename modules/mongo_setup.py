505bb645107f8edb7223ef27a5fd2ec39499a96a
Abhishek Bahuguna <142893756+Abhishek980-tech@users.noreply.github.com>
2026-05-04 21:00:14 +0530

    mongo_setup.py

--- modules/mongo_setup.py
+++ modules/mongo_setup.py
@@ -1,44 +1,9 @@
 
+
 import sys
 from pymongo.errors import PyMongoError
-
-
 from modules.db import get_db
-
-
 from .db import get_db
-
-from modules.db import get_db
-
-
-import sys
-from pymongo.errors import PyMongoError
-
-
-from modules.db import get_db
-
-
-import sys
-from pymongo.errors import PyMongoError
-
-from modules.db import get_db
-
-
-
-
-import sys
-from pymongo.errors import PyMongoError
-
-from modules.db import get_db
-
-
-
-
-import sys
-from pymongo.errors import PyMongoError
-
-from modules.db import get_db
-
 
 def ensure_indexes() -> bool:
     """Create required MongoDB indexes. Returns True on success."""
